4c4f72601756760368bda7fc98cb4450c9accef2
Abdulla <mohammad.abdulla@dxc.com>
2022-09-08 16:32:01 +0530

    JPA H2 Crud

---
+++ src/main/java/com/examp/controller/MemberController.java
@@ -0,0 +1,72 @@
+package com.examp.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examp.model.MemberInfo;
+import com.examp.repository.MemberRepository;
+
+@RestController
+public class MemberController {
+	
+	@Autowired
+	MemberRepository memberRepository;
+
+	@PostMapping("/member/add")
+	public ResponseEntity<MemberInfo> addUser(@RequestBody MemberInfo member) {
+		
+		try {
+			MemberInfo memberInfo = memberRepository.save(new MemberInfo(member.getName(), member.getType()));
+			return new ResponseEntity<>(memberInfo, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+	
+	@GetMapping("/member/get/{name}")
+	public ResponseEntity<List<MemberInfo>> findByName(@PathVariable String name) {
+		try {
+			List<MemberInfo> memberList = memberRepository.findByNameContainingIgnoreCase(name);
+			if (memberList.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(memberList, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/member/getById/{id}")
+	public ResponseEntity<MemberInfo> getTutorialById(@PathVariable int id) {
+		Optional<MemberInfo> member = memberRepository.findById(id);
+		if (member.isPresent()) {
+			return new ResponseEntity<>(member.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping("/member/get")
+	public ResponseEntity<List<MemberInfo>> getAll() {
+		try {
+			List<MemberInfo> memberList = memberRepository.findAll();
+			if (memberList.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(memberList, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+}
---
+++ src/main/java/com/examp/model/MemberInfo.java
@@ -0,0 +1,61 @@
+package com.examp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MemberInfo")
+public class MemberInfo {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(name = "Name")
+	private String name;
+	
+	//like admin or user
+	@Column(name = "Type")
+	private String type;
+
+	public MemberInfo() {
+	}
+
+	public MemberInfo(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberInfo [id=" + id + ", name=" + name + ", type=" + type + "]";
+	}
+	
+}
